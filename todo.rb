0ad5119f1edbcdde2186dbd06538f2cbe443aa1d
yash-learner <yash.topaz@gmail.com>
2021-05-21 16:27:33 +0530

    A new era of todo lists

--- todo.rb
+++ todo.rb
@@ -1,3 +1,12 @@
+# Write a comment
+# puts and gets
+# \n\n carriage return
+# String Interpolation
+# variables
+
+
+
+
 puts "What is your next todo:?"
 next_todo = gets
 
@@ -5,3 +14,6 @@ next_todo = gets
 # puts next_todo
 puts "\n\n\n"
 puts "[] #{next_todo}"
+
+
+# --- This is the begining of new era of todo lists
