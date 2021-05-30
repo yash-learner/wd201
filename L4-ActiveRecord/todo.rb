03b086b86b6c0b479f0316455e4752ed98de47da
yash-learner <yash.topaz@gmail.com>
2021-05-30 23:45:58 +0530

    Added mark_as_complete(id) and add_task(todo) class methods in todo.rb

--- L4-ActiveRecord/todo.rb
+++ L4-ActiveRecord/todo.rb
@@ -14,4 +14,18 @@ class Todo < ActiveRecord::Base
   def self.to_displayable_list
     all.map { |todo| todo.to_displayable_string }
   end
+
+  def self.show_list
+  end
+
+  def self.add_task(todo)
+    Todo.create!(todo_text: todo[:todo_text], due_date: Date.today + todo[:due_in_days], completed: false)
+  end
+
+  def self.mark_as_complete(id)
+    todo = Todo.find(id)
+    todo.completed = true
+    todo.save
+    todo
+  end
 end
